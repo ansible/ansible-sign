2833f8cc8cdcc7b25cea87749a921739d9bdd14b
Rick Elrod <rick@elrod.me>
2022-08-02 21:28:57 -0400

    nix some scaffolding commentary
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- src/ansible_signatory/cli.py
+++ src/ansible_signatory/cli.py
@@ -1,25 +1,3 @@
-"""
-This is a skeleton file that can serve as a starting point for a Python
-console script. To run this script uncomment the following lines in the
-``[options.entry_points]`` section in ``setup.cfg``::
-
-    console_scripts =
-         fibonacci = ansible_signatory.skeleton:run
-
-Then run ``pip install .`` (or ``pip install -e .`` for editable mode)
-which will install the command ``fibonacci`` inside your current environment.
-
-Besides console scripts, the header (i.e. until ``_logger``...) of this file can
-also be used as template for Python modules.
-
-Note:
-    This file can be renamed depending on your needs or safely removed if not needed.
-
-References:
-    - https://setuptools.pypa.io/en/latest/userguide/entry_point.html
-    - https://pip.pypa.io/en/stable/reference/pip_install
-"""
-
 import argparse
 import logging
 import os
